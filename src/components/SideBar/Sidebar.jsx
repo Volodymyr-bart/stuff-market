2e7c0cf2366e142afb4ce6208defd44d3242e56c
Volodymyr-bart <61430064+Volodymyr-bart@users.noreply.github.com>
2023-07-11 23:41:18 +0300

    finish sidebar

--- src/components/SideBar/Sidebar.jsx
+++ src/components/SideBar/Sidebar.jsx
@@ -1,6 +1,31 @@
+import { NavLink } from "react-router-dom";
 import styles from "./../../styles/Sidebar.module.css";
 const Sidebar = () => {
-  return <div className={styles.sidebar }></div>;
+  const id = 1;
+  return (
+    <section className={styles.sidebar}>
+      <div className={styles.title}>Categories</div>
+      <nav>
+        <ul className={styles.menu}>
+          <li>
+            <NavLink to={`/categories/${id}`}></NavLink>
+          </li>
+        </ul>
+      </nav>
+      <div className={styles.footer}>
+        <a href="#" target="_blank" className={styles.link}>
+          Help
+        </a>
+        <a
+          href="#"
+          target="_blank"
+          className={styles.link}
+          style={{ textDecoration: "underline" }}>
+          Terms &Conditions
+        </a>
+      </div>
+    </section>
+  );
 };
 
 export default Sidebar;
