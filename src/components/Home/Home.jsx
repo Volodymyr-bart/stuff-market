01794560e98f4f335e681192847dc0454e4c3129
Volodymyr-bart <61430064+Volodymyr-bart@users.noreply.github.com>
2023-07-13 23:03:00 +0300

    Create Banner

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,16 +2,17 @@ import { useSelector } from "react-redux";
 import Poster from "../Poster/Poster";
 import Products from "../Products/Products";
 import Categories from "../Categories/Categories";
+import Banner from "../Banner/Banner";
 
 const Home = () => {
   const { products, categories } = useSelector((state) => state);
-  console.log(categories.list);
 
   return (
     <>
       <Poster />
       <Products products={products.list} amount={5} title="Trending" />
       <Categories products={categories.list} amount={5} title="Worth seeing" />
+      <Banner/>
     </>
   );
 };
